c6cf6104793767e6ded423c9bc9229876ef69e20
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2024-03-13 21:11:46 -0400

    set warning alert message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
-import { InfoAlert, ErrorAlert } from "./components/Alert";
+import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 import "./App.css";
 
 const App = () => {
@@ -13,8 +13,14 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
+    if (navigator.onLine) {
+      setWarningAlert("");
+    } else {
+      setWarningAlert("The app is currently offline");
+    }
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -30,9 +36,12 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1>Meet App</h1>
+      <p>Choose your nearest city</p>
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <CitySearch
         allLocations={allLocations}
